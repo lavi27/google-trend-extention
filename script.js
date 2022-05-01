c2dbb60373c5c16ccd7a96c87efb614e6106e8a1
lavi <npt1237@gmail.com>
2022-05-01 23:03:43 +0900

    로직 완성

--- script.js
+++ script.js
@@ -4,21 +4,46 @@ document.addEventListener("DOMContentLoaded", () => {
     const nextDate = document.getElementsByClassName("nextDate")[0];
     const dateInput = document.getElementsByClassName("dateInput")[0];
     const lodingUI = document.getElementsByClassName("lodingUI")[0];
+    const today = document.getElementsByClassName("today")[0];
 
-    let date = [new Date().getFullYear(), (new Date().getMonth()+1).toString().padStart(2, "0"), new Date().getDate().toString().padStart(2, "0")];
-    dateInput.value = `${date[0]}-${date[1]}-${date[2]}`;
-    dateInput.min = `${date[0]}-${date[1]}-01`;
-    dateInput.max = `${date[0]}-${date[1]}-${date[2]}`;
+    let date, yr, mh, dy;
 
-    function getList(date) {
+    function now() {
+        return new Date();
+    }
+
+    function setDate(value) {
+        date = value;
+        yr = date.getFullYear();
+        mh = date.getMonth() + 1;
+        dy = date.getDate();
+    }
+
+    function Zfill(value) {
+        return value.toString().padStart(2, "0");
+    }
+
+    function getList() {
         wordList.innerHTML = "";
         lodingUI.style.display = "block";
 
-        fetch(`http://127.0.0.1:3000/api/main/${date[0]}/${date[1]}/${date[2]}`)
+        if (new Date(new Date(date).setDate(date.getDate() - 1)) > new Date(now().setDate(now().getDate() - 29))) {
+            previousDate.className = "previousDate";
+        } else {
+            previousDate.className = "previousDate block";
+        }
+
+        if (new Date(new Date(date).setDate(date.getDate() + 1)) < now()) {
+            nextDate.className = "nextDate";
+        } else {
+            nextDate.className = "nextDate block";
+        }
+
+        fetch(`http://127.0.0.1:3050/api/main/${yr}/${mh}/${dy}`)
             .then((response) => response.json())
             .then((data) => {
                 lodingUI.style.display = "none";
-
+                wordList.innerHTML = "";
                 data.forEach((item, count) => {
                     wordList.innerHTML += `<li><p class="ranking">${count + 1}</p> <span class="title">${item.title}</span><p class="traffic">${item.traffic.replace("K", "천") }</p></li>`;
                 });
@@ -26,24 +51,42 @@ document.addEventListener("DOMContentLoaded", () => {
             .catch(error => console.error(error))
     }
 
-    getList(date);
+    setDate(new Date(now().setDate(now().getDate() - 28)));
+
+    dateInput.min = `${yr}-${Zfill(mh)}-${Zfill(dy)}`;
+
+    setDate(new Date(now().setDate(now().getDate())));
+
+    dateInput.value = `${yr}-${Zfill(mh)}-${Zfill(dy)}`;
+    dateInput.max = `${yr}-${Zfill(mh)}-${Zfill(dy)}`;
+    getList();
 
     previousDate.addEventListener('click', () => {
-        date[2]--;
-        dateInput.value = `${date[0]}-${date[1]}-${date[2]}`
-        getList(date);
-    });
-
-    nextDate.addEventListener('click', () => {
-        if (date[2] < new Date().getDate()-1) {
-            date[2]++;
-            dateInput.value = `${date[0]}-${date[1]}-${date[2]}`
-            getList(date);
+        if (new Date(new Date(date).setDate(date.getDate() - 1)) > new Date(now().setDate(now().getDate() - 29))) {
+            setDate(new Date(new Date(date).setDate(date.getDate() - 1)));
+            dateInput.value = `${yr}-${Zfill(mh)}-${Zfill(dy)}`;
+            getList();
         }
     });
 
+    nextDate.addEventListener('click', () => {
+        if (new Date(new Date(date).setDate(date.getDate() + 1)) < now()) {
+            setDate(new Date(new Date(date).setDate(date.getDate() + 1)));
+            dateInput.value = `${yr}-${Zfill(mh)}-${Zfill(dy)}`;
+            getList();
+        }
+    });
+
+    today.addEventListener('click', () => {
+        setDate(now());
+        dateInput.value = `${yr}-${Zfill(mh)}-${Zfill(dy)}`;
+        getList();
+    });
+
     dateInput.addEventListener('change', (event) => {
-        date = event.target.value.split('-');
-        getList(date);
+        [yr, mh, dy] = event.target.value.split('-');
+        dy = parseInt(dy)
+        setDate(new Date(event.target.value));
+        getList();
     })
 });
